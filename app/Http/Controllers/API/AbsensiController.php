d3a8db9a115b6ce26f657a3ef356e390b32f630e
TubagusAfif <112999424+TubagusAfif@users.noreply.github.com>
2023-09-24 07:46:23 +0700

    update bug absensi action button

--- app/Http/Controllers/API/AbsensiController.php
+++ app/Http/Controllers/API/AbsensiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Absensi;
+use Illuminate\Support\Facades\Validator;
 
 
 class AbsensiController extends Controller
@@ -12,8 +13,15 @@ class AbsensiController extends Controller
     //
     public function hadir(Request $request){
         $userId = auth()->user()->no_anggota;
-        $kegiatanId = $request->input('id_kegiatan');
+        $validator = Validator::make($request->all(), [
+            'id_kegiatan' => 'required',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $kegiatanId = $request->input('id_kegiatan');
         $absen = Absensi::where('id_anggota', $userId)->where('id_kegiatan', $kegiatanId)->update(['status_absensi' => '1']);
 
         return response()->json([
