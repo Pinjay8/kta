6c84167547f441c6fc48377b40a0a88a764dca2c
TubagusAfif <112999424+TubagusAfif@users.noreply.github.com>
2023-09-26 13:48:23 +0700

    update build

---
+++ public/build/assets/ApplicationLogo-b88b886a.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-023fb78c.js";function r(s){return o.jsx("div",{children:o.jsx("img",{src:"img/img_logo.svg",className:"h-10 w-auto",alt:"logo"})})}export{r as A};
--- public/build/assets/ApplicationLogo-e26e4d51.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-65613a3b.js";function r(s){return o.jsx("div",{children:o.jsx("img",{src:"img/img_logo.svg",className:"h-10 w-auto",alt:"logo"})})}export{r as A};
