5eb3a1f431ee625e848ba63d1501fc950ed5d54f
TubagusAfif <112999424+TubagusAfif@users.noreply.github.com>
2023-10-05 10:39:14 +0700

    update build

---
+++ public/build/assets/ApplicationLogo-1cbbb8f8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-58fc4835.js";function i(s){return o.jsx("div",{children:o.jsx("img",{src:"http://localhost:8000/img/img_logo.svg",className:"h-10 w-auto",alt:"logo"})})}export{i as A};
