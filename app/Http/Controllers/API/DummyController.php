a5248b9033e1c100b356b5105dc3ee847d0740ce
TubagusAfif <112999424+TubagusAfif@users.noreply.github.com>
2023-10-06 18:05:49 +0700

    update dummy data

--- app/Http/Controllers/API/DummyController.php
+++ app/Http/Controllers/API/DummyController.php
@@ -14,10 +14,42 @@ class DummyController extends Controller
             'status' => true,
             'message' => 'success',
             'data' => [
+            [
                 'id' => '1',
                 'nama_dapil' => 'HJ TENNY JULIAWATY, S.E.,M.Si',
                 'dapil' => '1',
                 'no_urut' => '1',
+            ],
+            [
+                'id' => '2',
+                'nama_dapil' => 'HASNA RAKHMATIKA NURUL AZMI',
+                'dapil' => '1',
+                'no_urut' => '2',
+            ],
+            [
+                'id' => '3',
+                'nama_dapil' => 'SINTHYA DEWI',
+                'dapil' => '1',
+                'no_urut' => '3',
+            ],
+            [
+                'id' => '4',
+                'nama_dapil' => 'GINANJAR EKA NUGRAHA',
+                'dapil' => '1',
+                'no_urut' => '4',
+            ],
+            [
+                'id' => '5',
+                'nama_dapil' => 'SUGENG RIYATNO',
+                'dapil' => '1',
+                'no_urut' => '5',
+            ],
+            [
+                'id' => '6',
+                'nama_dapil' => 'HARLINDA S.H',
+                'dapil' => '1',
+                'no_urut' => '6',
+            ],
             ]
         ],200);
     }
