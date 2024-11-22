4897105b52f513c56e029c75fb4b52e5d60542f6
Tubagus Afif <tubagusafifnurhidayat0@gmail.com>
2024-11-22 18:34:44 +0700

    update build

--- public/build/assets/ApplicationLogo-1cbbb8f8.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-58fc4835.js";function i(s){return o.jsx("div",{children:o.jsx("img",{src:"http://localhost:8000/img/img_logo.svg",className:"h-10 w-auto",alt:"logo"})})}export{i as A};
