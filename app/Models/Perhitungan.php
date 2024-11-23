19817f073f552144cf1ede398da08dfbde5f9e84
Tubagus Afif <tubagusafifnurhidayat0@gmail.com>
2024-11-23 09:12:25 +0700

    feat: add soft delete

--- app/Models/Perhitungan.php
+++ app/Models/Perhitungan.php
@@ -4,15 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Perhitungan extends Model
 {
+    use SoftDeletes;
     use HasFactory;
 
     protected $fillable = [
         'id_kegiatan',
         'id_anggota',
         'id_tps',
+        'id_calon',
         'jumlah_dpt',
         'pemilih_hadir',
         'suara_sah',
