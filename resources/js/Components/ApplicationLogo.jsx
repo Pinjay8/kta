7e5b013ea7b60a301e1718462f1be3b992acb269
Alvin Cennanda <alvincennanda@gmail.com>
2024-11-25 15:17:06 +0700

    fix: logo pdip

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,11 +1,9 @@
+import Logo from "../../../public/img/img_logo.svg";
+
 export default function ApplicationLogo(props) {
     return (
         <div className="flex justify-center items-center w-100">
-            <img
-                src="http://localhost:8000/img/img_logo.svg"
-                className="h-24"
-                alt="logo"
-            />
+            <img src={Logo} className="h-24" alt="logo" />
         </div>
     );
 }
