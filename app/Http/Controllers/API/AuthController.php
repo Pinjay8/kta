c1ac5fa07aa2937209af0b53c04abae79be6c722
Tubagus Afif <tubagusafifnurhidayat0@gmail.com>
2024-11-25 11:58:58 +0700

    feat: add validation when user has logged in in another device

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -26,6 +26,10 @@ class AuthController extends Controller
             ], 401);
         }
 
+        if(Auth::guard('anggota')->user()->tokens()){
+            Auth::guard('anggota')->user()->tokens()->delete();
+        }
+
         $user = Anggota::where('no_hp', $request->no_hp)->firstOrFail();
         //GENERATE TOKEN SETELAH LOGIN
         $token = $user->createToken('auth_token')->plainTextToken;
