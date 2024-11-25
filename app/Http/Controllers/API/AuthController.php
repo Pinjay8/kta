93e8aefc71d4147db57fe274e52068580763c543
TubagusAfif <112999424+TubagusAfif@users.noreply.github.com>
2024-11-25 12:59:53 +0700

    Update AuthController.php

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -77,7 +77,7 @@ class AuthController extends Controller
                         'kelurahan' => $anggota->kelurahan ?? '',
                         'rt' => $anggota->rt ?? '',
                         'rw' => $anggota->rw ?? '',
-                        'no_cs' => '+6285640555861'
+                        'no_cs' => '+6288228883905'
                     ],
                     'tps' =>  [
                         'id' => $tps->id ?? '',
